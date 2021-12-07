d9a6d47769592d0f2779cb95c8419185c47ac864
Javier-Acosta <jacosta@catamarca.gov.ar>
2021-12-07 19:56:32 -0300

    PanelAdmin

--- webpersonal/settings.py
+++ webpersonal/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'portfolio.apps.PortfolioConfig',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +126,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Media files
+MEDIA_URL=  '/media/'
+MEDIA_ROOT=  os.path.join(BASE_DIR, 'media')
+# BASE_DIR / 'media',
--- webpersonal/urls.py
+++ webpersonal/urls.py
@@ -13,10 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django import conf
 from django.contrib import admin
 from django.urls import path
 from core import views
 
+from django.conf import settings
 
 urlpatterns = [
     path ('', views.home, name="home"),
@@ -25,3 +27,7 @@ urlpatterns = [
     path ('porfolio/', views.porfolio, name="porfolio"),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
